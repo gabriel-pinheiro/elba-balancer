e9f4035e00d4946d3d84c216b91d6e4bf823af06
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2021-08-28 22:08:07 -0300

    Ignoring default 404 when a global service is defined

--- src/elba/elba.router.ts
+++ src/elba/elba.router.ts
@@ -1,4 +1,5 @@
 import { ServerRoute } from "@hapi/hapi";
+import { configProvider } from "../config/config.service";
 import { Provider } from "../utils/decorators/provider";
 import { IRouter } from "../utils/router";
 import { ElbaController } from "./elba.controller";
@@ -10,20 +11,27 @@ export class ElbaRouter implements IRouter {
     ) { }
 
     async getRoutes(): Promise<ServerRoute[]> {
-        return [{
-            method: "GET",
-            path: "/__elba__/health",
-            handler: (req, h) => this.controller.health(req, h),
-            rules: {
-                mapSilently: true,
-            },
-        }, {
+        const generic404Route = {
             method: "*",
             path: "/{any*}",
             handler: (req, h) => this.controller.notFound(req, h),
             rules: {
                 mapSilently: true,
             },
+        };
+        const routes: ServerRoute[] = [{
+            method: "GET",
+            path: "/__elba__/health",
+            handler: (req, h) => this.controller.health(req, h),
+            rules: {
+                mapSilently: true,
+            },
         }];
+
+        if(!configProvider.config.service.some(s => ['', '*'].includes(s.host))) {
+            routes.push(generic404Route);
+        }
+
+        return routes;
     }
 }
