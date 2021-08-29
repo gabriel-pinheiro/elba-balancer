1475b9a0466d92ef723759d713d3a3d5e6c51e9a
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2021-08-28 23:14:28 -0300

    Refactoring request handler

--- src/request/request.handler.ts
+++ src/request/request.handler.ts
@@ -7,8 +7,9 @@ import { Attempt } from './attempt';
 import { ServiceTargetConfig } from '../config/data/config';
 import { MetricsService } from '../metrics/metrics.service';
 
-// Loop prevention
-const HARD_LIMIT = 20;
+const HARD_LIMIT = 20; // Retry loop prevention
+const ATTEMPT_HEADER = 'x-elba-attempts';
+const TARGET_HEADER = 'x-elba-target';
 
 export class RequestHandler {
     private readonly attempts: Attempt[] = [];
@@ -29,99 +30,39 @@ export class RequestHandler {
             let error = null;
             const response = await this.attempt()
                     .catch(e => error = e);
-            const targetName = this.attempts[this.attempts.length - 1]?.target?.name || 'elba';
 
-            // Downstream response
-            if(!error && !this.mustRetryResponse(response)) {
-                this.logger.info(`request completed`, {
-                    delay: new Date().getTime() - this.startTime.getTime(),
-                    topic: 'downstream-response',
-                    target: targetName,
-                    attempt: this.attempts.length,
-                });
-                this.metricsService.getDownstreamValue('downstream_success', this.upstream.config.host).add(1);
-                this.upstream.markTargetSuccess(targetName);
-                return response
-                        .header('x-elba-attempts', this.attempts.length)
-                        .header('x-elba-target', targetName);
+            const shouldRetry = response && this.mustRetryResponse(response) || error && this.mustRetryError(error);
+            const canRetry = !this.isRetryLimitReached();
+
+            if(shouldRetry && canRetry) {
+                await this.onRetry(error, response);
+                continue;
             }
 
-            // Downstream error (retryable or not, limit reached so we return it) or retryable response (limit reached so we return it)
-            if(this.isRetryLimitReached()) {
-                this.logger.error(`downstream request failed after limit with ${error ? `error: ${error}` : `status ${response.statusCode}`}`, {
-                    delay: new Date().getTime() - this.startTime.getTime(),
-                    topic: 'downstream-error',
-                    target: targetName,
-                    attempt: this.attempts.length,
-                });
-
-                if(error) {
-                    if(this.mustRetryError(error)) {
-                        this.metricsService.getDownstreamValue('downstream_error', this.upstream.config.host).add(1);
-                        this.upstream.markTargetFailure(targetName);
-                    } else {
-                        this.metricsService.getDownstreamValue('downstream_success', this.upstream.config.host).add(1);
-                    }
-
-                    if(error.isBoom) {
-                        error.output.headers['x-elba-attempts'] = this.attempts.length;
-                        error.output.headers['x-elba-target'] = targetName;
-                    }
-
-                    throw error;
-                } else {
-                    // No need to check for retry response, checked before
-                    this.metricsService.getDownstreamValue('downstream_error', this.upstream.config.host).add(1);
-                    this.upstream.markTargetFailure(targetName);
-                    return response
-                        .header('x-elba-attempts', this.attempts.length)
-                        .header('x-elba-target', targetName);
-                }
+            if(shouldRetry) {
+                this.onDownstreamError(error, response); // Should retry but can't
+            } else {
+                this.onDownstreamResponse();             // Shouldn't retry
             }
 
-            // Downstream unretryable error
-            if(error && !this.mustRetryError(error)) {
-                this.logger.error(`downstream request failed with error: ${error}`, {
-                    delay: new Date().getTime() - this.startTime.getTime(),
-                    topic: 'downstream-error',
-                    target: targetName,
-                    attempt: this.attempts.length,
-                });
-                this.metricsService.getDownstreamValue('downstream_error', this.upstream.config.host).add(1);
-
-                if(error.isBoom) {
-                    error.output.headers['x-elba-attempts'] = this.attempts.length;
-                    error.output.headers['x-elba-target'] = targetName;
-                }
-
-                throw error;
+            if(error) {
+                throw this.withElbaHeaders(error);
+            } else {
+                return this.withElbaHeaders(response);
             }
-
-            // Retry
-            this.logger.warn(`upstream request failed with ${error ? `error: ${error}` : `status ${response.statusCode}`}`, {
-                delay: new Date().getTime() - this.startTime.getTime(),
-                topic: 'upstream-error',
-                attempt: this.attempts.length,
-            });
-            this.upstream.markTargetFailure(targetName);
-            await this.delay();
         }
         
-        // Should not happen
+        // Hopefully never happens :)
+        this.logger.error('Too many retries, failing request');
         this.metricsService.getDownstreamValue('downstream_error', this.upstream.config.host).add(1);
-        throw Boom.serverUnavailable('All attempts failed');
+        throw this.withElbaHeaders(Boom.serverUnavailable('All attempts failed'));
     }
 
     private async attempt(): Promise<Hapi.ResponseObject> {
-        const target = await this.getSuitableTarget();
+        const target = this.getSuitableTarget();
         await this.cooldown(target);
         this.attempts.push(Attempt.of(target));
-
-        this.logger.debug(`attempting to proxy to ${target.name}`, {
-            attempt: this.attempts.length,
-            delay: new Date().getTime() - this.startTime.getTime(),
-            topic: 'upstream-request',
-        });
+        this.logStep('debug', `attempting to proxy to ${target.name}`, 'upstream-request');
         
         const response = await this.h.proxy({
             uri: this.buildEntrypointPath(target.url) + this.req.url.search,
@@ -135,22 +76,8 @@ export class RequestHandler {
         return response;
     }
 
-    private async getSuitableTarget(): Promise<ServiceTargetConfig> {
-        let targets = this.upstream.availableTargets;
-
-        if(targets.length === 0) {
-            if(this.upstream.config.health.none_healthy_is_all_healthy) {
-                this.logger.debug(`no healthy targets but health.none_healthy_is_all_healthy is true, choosing an unavailable one`);
-                targets = this.upstream.targets;
-            } else {
-                throw Boom.serverUnavailable("request failed because all the targets are unhealthy and health.none_healthy_is_all_healthy is unset or false", {
-                    options: {
-                        data: { isRetryable: false },   
-                    }
-                });
-            }
-        }
-
+    private getSuitableTarget(): ServiceTargetConfig {
+        const targets = this.getAvailableTargets();
         const attemptedTargetNames = this.attempts.map(a => a.target.name);
         const unattemptedTargets = targets.filter(t => !attemptedTargetNames.includes(t.name));
 
@@ -161,6 +88,24 @@ export class RequestHandler {
 
         // Sending request to the target that has been attempted the farthest from now
         return targets.reduce((a, b) => attemptedTargetNames.lastIndexOf(a.name) <= attemptedTargetNames.lastIndexOf(b.name) ? a : b);
+    }
+
+    private getAvailableTargets(): ServiceTargetConfig[] {
+        const targets = this.upstream.availableTargets;
+        if(targets.length > 0) {
+            return targets;
+        }
+
+        if(this.upstream.config.health.none_healthy_is_all_healthy) {
+            this.logger.debug(`no healthy targets but health.none_healthy_is_all_healthy is true, choosing an unavailable one`);
+            return this.upstream.targets;
+        }
+
+        throw Boom.serverUnavailable("request failed because all the targets are unhealthy and health.none_healthy_is_all_healthy is unset or false", {
+            options: {
+                data: { isRetryable: false },   
+            }
+        });
     }
 
     private mustRetryError(error: any): boolean {
@@ -226,6 +171,51 @@ export class RequestHandler {
             this.logger.debug(`waiting ${delay}ms before next attempt`, { topic: 'delay' });
             await Hoek.wait(delay);
         }
+    }
+
+    private withElbaHeaders(obj: any): any {
+        if(typeof obj.header === 'function') {
+            return obj.header(ATTEMPT_HEADER, this.attempts.length)
+                      .header(TARGET_HEADER, this.lastTargetName);
+        }
+
+        if(obj.isBoom) {
+            obj.output.headers[ATTEMPT_HEADER] = this.attempts.length;
+            obj.output.headers[TARGET_HEADER] = this.lastTargetName;
+        }
+
+        return obj;
+    }
+
+    private onDownstreamResponse(): void {
+        this.logStep('info', 'request completed', 'downstream-response');
+        this.metricsService.getDownstreamValue('downstream_success', this.upstream.config.host).add(1);
+        this.upstream.markTargetSuccess(this.lastTargetName);
+    }
+
+    private onDownstreamError(error: any, response: Hapi.ResponseObject): void {
+        this.logStep('error', `downstream request failed after limit with ${error ? `error: ${error}` : `status ${response.statusCode}`}`, 'downstream-error');
+        this.metricsService.getDownstreamValue('downstream_error', this.upstream.config.host).add(1);
+        this.upstream.markTargetFailure(this.lastTargetName);
+    }
+
+    private async onRetry(error: any, response: Hapi.ResponseObject) {
+        this.logStep('warn', `upstream request failed with ${error ? `error: ${error}` : `status ${response.statusCode}`}`, 'upstream-error');
+        this.upstream.markTargetFailure(this.lastTargetName);
+        await this.delay();
+    }
+
+    private logStep(severity: 'debug' | 'info' | 'warn' | 'error', message: string, topic: string) {
+        this.logger[severity](message, {
+            delay: new Date().getTime() - this.startTime.getTime(),
+            topic,
+            target: this.lastTargetName,
+            attempt: this.attempts.length,
+        });
+    }
+
+    private get lastTargetName(): string {
+        return this.attempts[this.attempts.length - 1]?.target?.name || 'elba';
     }
 
     private buildEntrypointPath(path: string): string {
